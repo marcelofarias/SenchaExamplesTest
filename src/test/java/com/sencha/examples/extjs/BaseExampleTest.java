b03db37314d795d3e7f0a93aed5ae500b32b2dfd
Marcelo Bukowski de Farias <marcelo.farias@sencha.com>
2014-03-17 23:24:47 -0700

    Desktop

---
+++ src/test/java/com/sencha/examples/extjs/BaseExampleTest.java
@@ -0,0 +1,71 @@
+/*
+ * Copyright (c) 2012-2013. Sencha Inc.
+ */
+package com.sencha.examples.extjs;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.runners.Parameterized;
+import org.openqa.selenium.remote.DesiredCapabilities;
+import org.openqa.selenium.remote.RemoteWebDriver;
+
+import java.net.URL;
+import java.util.Arrays;
+import java.util.List;
+
+public abstract class BaseExampleTest {
+
+    public BaseExampleTest(String browser) {
+        _browser = browser;
+    }
+
+    @Parameterized.Parameters
+    public static List data() {
+        return Arrays.asList(new Object[][]{
+                {"chrome"}
+        });
+    }
+    
+    private URL getWebDriverHubURL() throws Exception {
+        String webDriverHub = System.getProperty("webdriver.hub");
+        return new URL(String.format("http://%s:4444/wd/hub", webDriverHub));
+    }
+
+    protected RemoteWebDriver getWebDriver(String browserName) throws Exception {
+        if ("chrome".equals(browserName)) {
+            return new RemoteWebDriver(
+                    getWebDriverHubURL(),
+                    DesiredCapabilities.chrome());
+        }
+        throw new RuntimeException(String.format("Unsupported browser", browserName));
+    }
+
+    protected List<String> getJavaScriptErrors() {
+        return (List<String>) getWebDriver().executeScript("return window.__webdriver_javascript_errors");
+    }
+    
+    protected RemoteWebDriver getWebDriver() {
+        return _webDriver;
+    }
+
+    protected abstract String getExamplePath();
+
+    @Before
+    public void openExemple() throws Exception {
+        String callbackAddress = System.getProperty("callback.address");
+        _webDriver = getWebDriver(_browser);
+        _webDriver.get(String.format(
+                    "http://%s:1841/ext/build/examples%s",
+                    callbackAddress,
+                    getExamplePath()));
+    }
+
+    @After
+    public void closeExample() throws Exception {
+        _webDriver.close();
+    }
+
+    private String _browser;
+    private RemoteWebDriver _webDriver;
+    
+}
